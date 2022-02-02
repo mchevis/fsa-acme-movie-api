6c761c3c852f847b9cb420402fcee9d2626e1c75
Marina Chevis <marinachevis@gmail.com>
2022-02-02 18:57:00 -0500

    first commit

---
+++ test/spec.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+const { expect } = require("chai");
+
+describe("This app has tests", function () {
+  it("ran this test", () => {
+    expect(true).to.equal(true);
+  });
+});
